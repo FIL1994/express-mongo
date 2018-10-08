3a225d00bb464df4c32f6924e98c8f6d4f13ea5f
Philip Van Raalte <phil.vanraalte@gmail.com>
2018-10-08 20:57:02 +0000

    added timestamps

--- models/Post.js
+++ models/Post.js
@@ -1,19 +1,24 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const postSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-    maxlength: [70, "Title cannot be more than 70 characters"]
+const postSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      maxlength: [70, "Title cannot be more than 70 characters"]
+    },
+    content: {
+      type: String,
+      required: true,
+      maxlength: [300, "Content cannot be more than 300 characters"]
+    },
+    author: { type: Schema.Types.ObjectId, ref: "User" }
   },
-  content: {
-    type: String,
-    required: true,
-    maxlength: [300, "Content cannot be more than 300 characters"]
-  },
-  author: { type: Schema.Types.ObjectId, ref: "User" }
-});
+  {
+    timestamps: true
+  }
+);
 
 const Post = mongoose.model("Post", postSchema, "posts");
 
