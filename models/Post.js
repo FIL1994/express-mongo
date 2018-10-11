e95fefb9413486cbe5a52a033f14479e0da68003
Philip Van Raalte <phil.vanraalte@gmail.com>
2018-10-11 01:04:57 +0000

    removed title field form posts and added location and imageURL fields

--- models/Post.js
+++ models/Post.js
@@ -3,17 +3,14 @@ const { Schema } = mongoose;
 
 const postSchema = new Schema(
   {
-    title: {
-      type: String,
-      required: true,
-      maxlength: [70, "Title cannot be more than 70 characters"]
-    },
     content: {
       type: String,
       required: true,
       maxlength: [300, "Content cannot be more than 300 characters"]
     },
-    author: { type: Schema.Types.ObjectId, ref: "User" }
+    author: { type: Schema.Types.ObjectId, ref: "User" },
+    location: String,
+    imageURL: String
   },
   {
     timestamps: true
